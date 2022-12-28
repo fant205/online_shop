16d6e4e849f94f6f3054a2871889b049ee86fe8e
fant205 <fant205@mail.ru>
2022-12-28 13:43:02 +0300

    fix

--- src/main/java/com/alexey/shop/repository/ProductsRepository.java
+++ src/main/java/com/alexey/shop/repository/ProductsRepository.java
@@ -4,6 +4,11 @@ import com.alexey.shop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductsRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    public Optional<Product> findByTitle(String title);
 
 }
